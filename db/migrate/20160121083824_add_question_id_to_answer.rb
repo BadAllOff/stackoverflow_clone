a111380ba44830e563b46ce4d673614f0f7f7cd7
Shamil BadAllOff <BadAllOffsh@gmail.com>
2016-01-21 17:28:01 +0400

    Change in DB schema - added index and foreign_key to migration

--- db/migrate/20160121083824_add_question_id_to_answer.rb
+++ db/migrate/20160121083824_add_question_id_to_answer.rb
@@ -1,5 +1,5 @@
 class AddQuestionIdToAnswer < ActiveRecord::Migration
   def change
-    add_column :answers, :question_id, :integer
+    add_belongs_to :answers, :question, index: true, foreign_key: true
   end
 end
