30a3361551ae6f325b047d23a2824958a1d7e47d
Shamil BadAllOff <BadAllOffsh@gmail.com>
2016-01-26 18:51:10 +0400

    Add: Authenticated and Non-authenticated users try to create and destroy answers

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -5,16 +5,17 @@ RSpec.describe AnswersController, type: :controller do
   let(:answer) { create(:answer, question: question) }
 
   describe 'POST #create' do
-    sign_in_user
-    context 'with valid attributes' do
-      it 'saves new answer in the database' do
-        expect{ post :create, answer: attributes_for(:answer), question_id: question }.to change(question.answers, :count).by(1)
+    context 'Authenticated user' do
+      sign_in_user
+      context 'with valid attributes' do
+        it 'saves new answer in the database' do
+          expect{ post :create, answer: attributes_for(:answer), question_id: question }.to change(question.answers, :count).by(1)
+        end
+        it 'redirect back to Question' do
+          post :create, answer: attributes_for(:answer), question_id: question
+          expect(response).to redirect_to question_path(question)
+        end
       end
-      it 'redirect back to Question' do
-        post :create, answer: attributes_for(:answer), question_id: question
-        expect(response).to redirect_to question_path(question)
-      end
-    end
 
       context 'with invalid attributes' do
         it 'does not save the answer' do
@@ -25,18 +26,37 @@ RSpec.describe AnswersController, type: :controller do
           expect(response).to redirect_to question_path(question)
         end
       end
+    end
+
+    context 'Non-authenticated user try to create answer' do
+      it 'redirects to login page' do
+        expect{ post :create, answer: attributes_for(:answer), question_id: question }.to_not change(question.answers, :count)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
   end
 
   describe 'DELETE #destroy' do
-    sign_in_user
-    before { answer }
-    it 'deletes answer' do
-      expect { delete :destroy, question_id: question, id: answer  }.to change(Answer, :count).by(-1)
+    context 'Authenticated user' do
+      sign_in_user
+      before { answer }
+      it 'deletes answer' do
+        expect { delete :destroy, question_id: question, id: answer  }.to change(Answer, :count).by(-1)
+      end
+
+      it 'redirect to question view' do
+        delete :destroy, question_id: question, id: answer
+        expect(response).to redirect_to question_path(question)
+      end
     end
 
-    it 'redirect to question view' do
-      delete :destroy, question_id: question, id: answer
-      expect(response).to redirect_to question_path(question)
+    context 'Non-authenticated user try to delete answer' do
+      before { answer }
+      it 'redirects to login page' do
+        expect { delete :destroy, question_id: question, id: answer  }.to_not change(Answer, :count)
+        expect(response).to redirect_to new_user_session_path
+      end
     end
   end
+
 end
