b1da2a3965f431ac5f848aee1fd4efdb06c8478b
Shamil BadAllOff <BadAllOffsh@gmail.com>
2016-02-10 09:57:30 +0400

    ADD: destroy attachment via ajax

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -1,29 +1,15 @@
 require 'rails_helper'
 
 RSpec.configure do |config|
-  config.include AcceptanceMacros, type: :feature
-
-  # Capybara config to avoid some annoying messages
-  Capybara::Webkit.configure do |config|
-    # Enable debug mode. Prints a log of everything the driver is doing.
-    config.debug = false
-
-    config.allow_unknown_urls
-    # Allow pages to make requests to any URL without issuing a warning.
-
-    # Allow a specifc domain without issuing a warning.
-    config.allow_url('placehold.it')
-
-    # Timeout if requests take longer than 5 seconds
-    config.timeout = 10
-
-    # Don't raise errors when SSL certificates can't be validated
-    config.ignore_ssl_errors
-  end
 
   Capybara.javascript_driver = :webkit
 
-  Capybara.default_max_wait_time = 5
+  Capybara::Webkit.configure do |config|
+    config.block_unknown_urls
+  end
+
+
+  config.include AcceptanceMacros, type: :feature
 
   config.use_transactional_fixtures = false
 
