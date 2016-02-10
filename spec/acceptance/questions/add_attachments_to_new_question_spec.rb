d80a4f80bf3b7582926acc44dd09a992a629683e
Shamil BadAllOff <BadAllOffsh@gmail.com>
2016-02-10 19:49:16 +0400

    Add: remove created field from attachable object form

--- spec/acceptance/questions/add_attachments_to_new_question_spec.rb
+++ spec/acceptance/questions/add_attachments_to_new_question_spec.rb
@@ -26,6 +26,19 @@ feature 'Add files to question', %q{
         expect(page).to have_link '10x10.jpg', href: '/uploads/attachment/file/1/10x10.jpg'
       end
 
+      scenario '- adds attachment when asks question, but changes his mind, deletes file field from form', js: true do
+        within('form#new_question') do
+          fill_in 'Title', with: 'Test question title'
+          fill_in 'Body', with: 'Test question body'
+          click_on 'Add file'
+          attach_file 'File', "#{Rails.root}/spec/fixtures/10x10.jpg"
+          click_on 'Remove file'
+          click_on 'Create Question'
+        end
+
+        expect(page).to_not have_link '10x10.jpg', href: '/uploads/attachment/file/1/10x10.jpg'
+      end
+
       scenario '- could add several files when asks question', js: true do
         within('form#new_question') do
           fill_in 'Title', with: 'Test question title'
