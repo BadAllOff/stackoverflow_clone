87f283df4250ef9f53ca62844158ac5ecde4a6e4
Shamil BadAllOff <BadAllOffsh@gmail.com>
2016-02-17 10:25:52 +0400

    Add: acceptance for votes

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -7,7 +7,7 @@ RSpec.configure do |config|
   Capybara::Webkit.configure do |config|
     config.block_unknown_urls
   end
-
+  Capybara.default_max_wait_time = 5
 
   config.include AcceptanceMacros, type: :feature
 
