2678462f0a019024281b5d58985f678f564d3179
Shamil BadAllOff <BadAllOffsh@gmail.com>
2016-02-26 19:26:41 +0400

    Add: user uniqueness in the scope BUT whithout test

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,9 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :votable, polymorphic: true
-  
+
   validates :value, presence: true
+
+  # TODO: - how to test this scope?
+  validates :user_id, uniqueness: { scope: [:votable_type, :votable_id] }
 end
