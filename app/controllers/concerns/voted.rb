a6aeb93a2d922193c56c7d98beea3eea228f8cfa
Shamil BadAllOff <BadAllOffsh@gmail.com>
2016-03-09 00:46:29 +0400

    Refactor: voted controller concern

--- app/controllers/concerns/voted.rb
+++ app/controllers/concerns/voted.rb
@@ -8,59 +8,53 @@ module Voted
 
   def upvote
     if current_user.author_of?(@votable)
-      flash[:error] = "You can't vote for your own answer"
+      flash[:error] = "You can't vote for your own #{model_klass}"
     else
       if current_user.voted_for?(@votable)
-        flash[:error] = 'You already voted up for this answer'
+        flash[:error] = 'You already voted up for this ' + model_klass.to_s
       else
-        flash[:success] = 'You have successfully voted up for answer'
+        flash[:success] = 'You have successfully voted up for ' + model_klass.to_s
         current_user.vote_for(@votable, 1)
       end
     end
 
     update_rating
 
-    respond_to do |format|
-      format.json { render 'vote' }
-    end
+    render 'vote'
   end
 
   def downvote
     if current_user.author_of?(@votable)
-      flash[:error] = "You can't vote for your own answer"
+      flash[:error] = "You can't vote for your own #{model_klass}"
     else
       if current_user.voted_for?(@votable)
-        flash[:error] = 'You already voted down for this answer'
+        flash[:error] = 'You already voted down for this ' + model_klass.to_s
       else
-        flash[:success] = 'You have successfully voted down for answer'
+        flash[:success] = 'You have successfully voted down for ' + model_klass.to_s
         current_user.vote_for(@votable, -1)
       end
     end
 
     update_rating
 
-    respond_to do |format|
-      format.json { render 'vote' }
-    end
+    render 'vote'
   end
 
   def unvote
     if current_user.author_of?(@votable)
-      flash[:error] = "You can't vote for your own answer"
+      flash[:error] = "You can't vote for your own " + model_klass.to_s
     else
       if current_user.voted_for?(@votable)
-        flash[:success] = 'Your vote has been deleted. You can revote now'
+        flash[:success] = 'Your vote has been deleted. You can re-vote now'
         current_user.unvote_for(@votable)
       else
-        flash[:error] = "You didn't yet vote for answer. There is nothing to reset"
+        flash[:error] = "You didn't yet vote for #{model_klass}. There is nothing to reset"
       end
     end
 
     update_rating
 
-    respond_to do |format|
-      format.json { render 'vote' }
-    end
+    render 'vote'
   end
 
   private
