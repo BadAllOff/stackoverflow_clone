842e1d5e306e285cd4df50d693586242c917fcfb
Shamil BadAllOff <BadAllOffsh@gmail.com>
2016-03-14 01:47:05 +0400

    Add: question voting final

--- spec/controllers/votes_concern_spec.rb
+++ spec/controllers/votes_concern_spec.rb
@@ -50,3 +50,55 @@ describe AnswersController do
     end
   end
 end
+
+
+describe QuestionsController do
+  let(:user) { create :user }
+  let(:another_user) { create :user }
+  let(:question) { create :question, user: user }
+
+  describe 'PATCH #upvote' do
+    context 'votes up for his own question' do
+      before { sign_in(user) }
+      it '- does not keep the vote' do
+        expect { patch :upvote, id: question, format: :json }.to_not change(question.votes.upvotes, :count)
+        expect { patch :upvote, id: question, format: :json }.to_not change(question.votes.upvotes, :size)
+      end
+    end
+
+    context "votes up for other user's question" do
+      before { sign_in(another_user) }
+      it "- keep's the vote" do
+        expect { patch :upvote, id: question, format: :json }.to change(question.votes.upvotes, :count).by 1
+      end
+    end
+  end
+
+  describe 'PATCH #downvote' do
+    context 'votes down for his own question' do
+      before { sign_in(user) }
+      it '- does not keep the vote' do
+        expect { patch :downvote, id: question, format: :json }.to_not change(question.votes.downvotes, :count)
+      end
+    end
+
+    context "votes down for other user's question" do
+      before { sign_in(another_user) }
+      it "- keep's the vote" do
+        expect { patch :downvote, id: question, format: :json }.to change(question.votes.downvotes, :count).by 1
+      end
+    end
+  end
+
+  describe 'PATCH #unvote' do
+    before do
+      sign_in(another_user)
+      patch :upvote, id: question, format: :json
+    end
+
+    it '- deletes vote for votable object (question)' do
+      expect { patch :unvote, id: question, format: :json }.to change(question.votes, :count).by(-1)
+    end
+  end
+
+end
