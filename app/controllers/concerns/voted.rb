09accd3091c8e53ebe996d9dc5450fefd1885200
Shamil BadAllOff <BadAllOffsh@gmail.com>
2016-04-27 14:13:01 +0400

    Refactoring: js & votable concern

--- app/controllers/concerns/voted.rb
+++ app/controllers/concerns/voted.rb
@@ -3,51 +3,27 @@ module Voted
   extend ActiveSupport::Concern
 
   included do
-    before_action :set_vote, only: [:upvote, :downvote, :unvote]
+    before_action :set_vote, :votes_for_own_votable, :already_voted, only: [:upvote, :downvote, :unvote]
+    # before_action :already_voted, only: [:upvote, :downvote]
+    after_action :discard_flash, only: [:upvote, :downvote, :unvote]
   end
 
   def upvote
-    if current_user.author_of?(@votable)
-      flash[:error] = "You can't vote for your own #{model_klass}"
-    else
-      if current_user.voted_for?(@votable)
-        flash[:error] = 'You already voted up for this ' + model_klass.to_s
-      else
-        flash[:success] = 'You have successfully voted up for ' + model_klass.to_s
-        current_user.vote_for(@votable, 1)
-      end
-    end
-
+    current_user.vote_for(@votable, 1)
     render 'vote'
   end
 
   def downvote
-    if current_user.author_of?(@votable)
-      flash[:error] = "You can't vote for your own #{model_klass}"
-    else
-      if current_user.voted_for?(@votable)
-        flash[:error] = 'You already voted down for this ' + model_klass.to_s
-      else
-        flash[:success] = 'You have successfully voted down for ' + model_klass.to_s
-        current_user.vote_for(@votable, -1)
-      end
-    end
-
+    current_user.vote_for(@votable, -1)
     render 'vote'
   end
 
   def unvote
-    if current_user.author_of?(@votable)
-      flash[:error] = "You can't vote for your own " + model_klass.to_s
-    else
-      if current_user.voted_for?(@votable)
-        flash[:success] = 'Your vote has been deleted. You can re-vote now'
-        current_user.unvote_for(@votable)
-      else
-        flash[:error] = "You didn't yet vote for #{model_klass}. There is nothing to reset"
-      end
-    end
+    if current_user.voted_for?(@votable)
 
+    else
+    end
+    current_user.unvote_for(@votable)
     render 'vote'
   end
 
@@ -60,6 +36,35 @@ module Voted
   def set_vote
     @votable = model_klass.find(params[:id])
     instance_variable_set("@#{controller_name.singularize}", @votable)
+  end
+
+  def votes_for_own_votable
+    if current_user.author_of?(@votable)
+      flash[:error] = "You can't vote for your own #{model_klass}"
+      render 'vote'
+    end
+  end
+
+  def already_voted
+    if current_user.voted_for?(@votable)
+      if action_name == 'unvote'
+        flash[:success] = 'Your vote has been deleted. You can re-vote now'
+      else
+        flash[:error] = 'You already voted for this ' + model_klass.to_s
+        render 'vote'
+      end
+    else
+      if action_name == 'unvote'
+        flash[:error] = "You didn't yet vote for #{model_klass}. There is nothing to reset"
+        render 'vote'
+      else
+        flash[:success] = 'You have successfully voted for ' + model_klass.to_s
+      end
+    end
+  end
+
+  def discard_flash
+    flash.discard
   end
 
 end
