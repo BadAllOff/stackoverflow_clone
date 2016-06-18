67b06de6a48448710c86bd6f02b6f801f32c3fbf
Badalloff <badalloffsh@gmail.com>
2016-06-18 18:43:25 +0400

    ADD: answers json create, update

--- app/assets/javascripts/answers.js
+++ app/assets/javascripts/answers.js
@@ -1,115 +1,156 @@
 var ready;
 
 ready = function() {
-  var userId;
-  var questionId;
-  var csrfToken = $("meta[name='csrf-token']").attr('content');
+    var userId;
+    var questionId;
+    var csrfToken = $("meta[name='csrf-token']").attr('content');
 
-  userId = $('#current_user_meta').data('userId');
-  questionId = $('#answers').data('questionId');
+    userId = $('#current_user_meta').data('userId');
+    questionId = $('#answers').data('questionId');
 
-  $(function() {
-    $('.cancel_update').bind('click', function(e) {
-      e.preventDefault();
-      var answerId = $(this).data('answerId');
+    function remove_alerts() {
+        setTimeout(function(){
+            $('.flash-messages > .alert').fadeOut('slow', function(){
+                $(this).remove();
+            });
+        }, 3500);
+    }
 
-      $('.form_for_answer-' + answerId).hide();
-      $('li#answer-'+ answerId +'btn-answer-edit').show();
-    });
-  });
+    $(function() {
+        $('.cancel_update').bind('click', function(e) {
+            e.preventDefault();
+            var answerId = $(this).data('answerId');
 
-  $(function() {
-    return $('.votes_answer').bind('ajax:success', function(e, data, status, xhr) {
-      var answer;
-
-      answer = $.parseJSON(xhr.responseText);
-      $("#answer-" + answer.id + " .votes_answer").html(JST["templates/votes"]({
-        object: answer
-      }));
-
-      setTimeout(function(){
-        $('.flash-messages > .alert').fadeOut('slow', function(){
-          $(this).remove();
+            $('.form_for_answer-' + answerId).hide();
+            $('li#answer-'+ answerId).find('.btn-answer-edit').show();
         });
-      }, 3500);
-
-      return $('.flash-messages').append(JST["templates/shared/msg"]({
-        object: answer
-      }));
     });
-  });
+
+
+    $(function() {
+        return $('.votes_answer').bind('ajax:success', function(e, data, status, xhr) {
+            var answer;
+
+            answer = $.parseJSON(xhr.responseText);
+            $("#answer-" + answer.id + " .votes_answer").html(JST["templates/votes"]({
+                object: answer
+            }));
+
+            remove_alerts();
+
+            return $('.flash-messages').append(JST["templates/shared/msg"]({
+                object: answer
+            }));
+        });
+    });
 
 
 
-  $(function() {
-    return $('#new_answer').unbind().bind('ajax:error', function(e, data, status, xhr) {
-      if (e.target.id == "new_answer") {
+    $(function() {
+        return $('#new_answer').unbind().bind('ajax:error', function(e, data, status, xhr) {
+            if (e.target.id == "new_answer") {
+                var answer;
+                var answerMessages = $('.answer-messages').first();
+                var errorsDiv;
+
+                answerMessages.empty();
+                answer = $.parseJSON(data.responseText);
+
+                errorsDiv = JST["templates/shared/errors"]({object: answer});
+                answerMessages.prepend(errorsDiv);
+
+                remove_alerts();
+
+                return $('.flash-messages').append(JST["templates/shared/msg"]({
+                    object: answer
+                }));
+            }
+        });
+    });
+
+    $(function() {
+        return $('.edit_answer').unbind().bind('ajax:error', function(e, data, status, xhr) {
+            var answer_id;
+            answer_id = $(this).find('.btn-info').data('answerId');
+            var answerMessages = $(this).find('.answer-messages');
+            answerMessages.empty();
+            answer = $.parseJSON(data.responseText);
+
+            errorsDiv = JST["templates/shared/errors"]({object: answer});
+            answerMessages.prepend(errorsDiv);
+            remove_alerts();
+
+            return $('.flash-messages').append(JST["templates/shared/msg"]({
+                object: answer
+            }));
+        });
+    });
+
+
+    function show_edit_answer_form() {
+        $('.btn-answer-edit').unbind().bind('click', function(e) {
+            var answer_id;
+            e.preventDefault();
+            answer_id = $(this).data('answerId');
+            $(this).hide();
+            return $('div.form_for_answer-' + answer_id).show();
+        });
+    }
+    show_edit_answer_form();
+
+    PrivatePub.subscribe('/questions/' + questionId + '/answers', function(data, channel) {
+
         var answer;
-        var answerMessages = $('.answer-messages').first();
-        var errorsDiv;
+        var newAnswerDiv;
+        var answerMessages = $('.answer-messages');
 
         answerMessages.empty();
-        answer = $.parseJSON(data.responseText);
 
-        errorsDiv = JST["templates/shared/errors"]({object: answer});
-        answerMessages.prepend(errorsDiv);
+        answer = $.parseJSON(data['answer']);
+        if (answer.author.author_id == userId) {
+            answer.currentUserIsAuthor = true;
+            $('#answer_body').val('');
+        }else{
+            answer.currentUserIsAuthor = false;
+        }
 
-        setTimeout(function () {
-          $('.flash-messages > .alert').fadeOut('slow', function () {
-            $(this).remove();
-          });
-        }, 3500);
+        if (answer.parentQuestionAuthorId == userId) {
+            answer.currentUserIsAuthorOfQuestion = true;
+        }else{
+            answer.currentUserIsAuthorOfQuestion = false;
+        }
+
+
+        if (answer.is_new) {
+            newAnswerDiv = JST["templates/answers/answer"]({object: answer});
+            $('.answers').find('ul.answers_list').prepend(newAnswerDiv);
+
+            setTimeout(function(){
+                $('.flash-messages > .alert').fadeOut('slow', function(){
+                    $(this).remove();
+                });
+            }, 3500);
+
+            if (!answer.currentUserIsAuthor){
+                answer.msgs.ok_msg = null;
+                answer.msgs.notice = " New answer was added";
+            }
+        }else{
+            updatedAnswerDiv = JST["templates/answers/answer"]({object: answer});
+            var liAnswerId = $('li#answer-'+answer.id);
+            liAnswerId.replaceWith(updatedAnswerDiv);
+
+            if (!answer.currentUserIsAuthor){
+                answer.msgs.ok_msg = null;
+                answer.msgs.notice = " Answer was updated";
+                $('li#answer-'+answer.id).find('.answer-body').addClass('bg-info');
+            }
+        }
 
         return $('.flash-messages').append(JST["templates/shared/msg"]({
-          object: answer
+            object: answer
         }));
-
-      }
     });
-  });
-
-
-  PrivatePub.subscribe('/questions/' + questionId + '/answers', function(data, channel) {
-
-    var answer;
-    var newAnswerDiv;
-    var answerMessages = $('.answer-messages');
-
-    answerMessages.empty();
-
-    answer = $.parseJSON(data['answer']);
-    if (answer.author.author_id == userId) {
-      answer.currentUserIsAuthor = true;
-      $('#answer_body').val('');
-    }else{
-      answer.currentUserIsAuthor = false;
-    }
-
-    if (answer.parentQuestionAuthorId == userId) {
-      answer.currentUserIsAuthorOfQuestion = true;
-    }else{
-      answer.currentUserIsAuthorOfQuestion = false;
-    }
-
-    newAnswerDiv = JST["templates/answers/answer"]({object: answer});
-    $('.answers').find('ul.answers_list').prepend(newAnswerDiv);
-
-    setTimeout(function(){
-      $('.flash-messages > .alert').fadeOut('slow', function(){
-        $(this).remove();
-      });
-    }, 3500);
-
-    if (!answer.currentUserIsAuthor){
-      answer.msgs.ok_msg = null;
-      answer.msgs.notice = " New answer was added";
-    }
-
-    return $('.flash-messages').append(JST["templates/shared/msg"]({
-      object: answer
-    }));
-  });
-
 };
 
 $(document).ready(ready);
