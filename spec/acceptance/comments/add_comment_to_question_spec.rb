b7c174ea641d64aff85972abeef6de1ae512ca94
Badalloff <badalloffsh@gmail.com>
2016-06-19 17:14:05 +0400

    Add: acceptance test for question comment

---
+++ spec/acceptance/comments/add_comment_to_question_spec.rb
@@ -0,0 +1,41 @@
+require_relative '../acceptance_helper'
+
+feature 'Add comment question' do
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question, user: user) }
+
+  describe 'Authenticated user' do
+    context 'creates new comment' do
+      before do
+        sign_in(user)
+        visit question_path(question)
+      end
+
+      scenario "create comment", js: true do
+        within ".question_comments" do
+          click_on "add a comment"
+          within 'form#new_comment' do
+            fill_in 'Comment content', with: 'Test question comments'
+            click_on 'Create comment'
+          end
+
+          expect(page).to have_content 'Test question comments'
+        end
+      end
+
+      scenario "show/hide form comment", js: true do
+        within ".question_comments" do
+          expect(page).to_not have_selector :css, 'form#new_comment'
+          click_on "add a comment"
+          expect(page).to have_selector :css, 'form#new_comment'
+          click_on  "hide comment form"
+          expect(page).to_not have_selector :css, 'form#new_comment'
+        end
+      end
+
+    end
+  end
+
+
+end
