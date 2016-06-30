45e933e7b94a79fb557b4fa8cbb76e682e6afb96
Badalloff <badalloffsh@gmail.com>
2016-06-30 20:06:18 +0400

    Fix: tests rewritten

--- spec/controllers/comments_contoller_spec.rb
+++ spec/controllers/comments_contoller_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
-  let!(:user)      { create(:user) }
-  let!(:question)  { create(:question, user: user) }
-  let!(:comment)   { create(:comment, commentable: question, user: user ) }
+  let!(:user)             { create(:user) }
+  let!(:question)         { create(:question, user: user) }
+  let!(:comment)          { create(:comment, commentable: question, user: user ) }
+  let!(:invalid_comment)  { create(:comment, commentable: question, user: user, content: '' ) }
 
   describe 'POST #create' do
     context 'Authenticated user' do
@@ -26,11 +27,11 @@ RSpec.describe CommentsController, type: :controller do
 
       context 'with invalid attributes' do
         it '- does not save the answer' do
-          expect{ post :create, answer: attributes_for(:invalid_answer), question_id: question, format: :json }.to_not change(Answer, :count)
+          expect{ post :create, comment: attributes_for(:invalid_comment), question_id: question, format: :json }.to_not change(Comment, :count)
         end
         it '- returns errors in json format' do
-          post :create, answer: attributes_for(:invalid_answer), question_id: question, format: :json
-          expect(response).to render_template 'answers/errors.json.jbuilder'
+          post :create, comment: attributes_for(:invalid_comment), question_id: question, format: :json
+          expect(response).to render_template 'comments/errors.json.jbuilder'
           expect(response.status).to eq 400
         end
       end
