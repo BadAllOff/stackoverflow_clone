78593cc93a83b88bb82467acc9fac36175645aa5
Badalloff <badalloffsh@gmail.com>
2016-07-11 14:18:49 +0400

    Add: refactoring comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -55,7 +55,7 @@ class CommentsController < ApplicationController
 
   def set_commentable
     params.each do |name, value|
-      if name =~ /(.+)_id$/
+      if name =~ /(question|answer)_id$/
         @commentable_name = $1
         @commentable = $1.classify.constantize.find(value)
       end
