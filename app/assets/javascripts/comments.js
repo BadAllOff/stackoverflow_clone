98960cd286d8b64a08ecd3ee5d48a03cecba7b53
Badalloff <badalloffsh@gmail.com>
2016-07-13 05:35:12 +0400

    Refactoring: Comment destroy JS files

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -5,92 +5,39 @@ ready = function() {
   var userId = $('#current_user_meta').data('userId');
   var questionId = $('#answers').data('questionId');
 
-  // question comment create
-  PrivatePub.subscribe('/questions/' + questionId + '/comments/create', function(data, channel) {
-    var comment = $.parseJSON(data['comment']);
-    var parentType = comment.relationships.commentable.commentable_type
-    var parentId = comment.relationships.commentable.commentable_id
-    var authorId = comment.relationships.author.author_id
+  function addCommentToCommentable(data){
 
-    authorId == userId ? comment.currentUserIsAuthor = true : comment.currentUserIsAuthor = false;
-
-    newCommentDiv = JST["templates/comments/comment"]({object: comment});
-
-    $('.new_comment_form_for_'+parentType).find('.commentMessages').html('');
-    $('.'+parentType+'_comments').append(newCommentDiv);
-
-    if (comment.currentUserIsAuthor) {
-      $('.'+parentType+'_'+parentId+'_comment_content').val('');
-
-      setTimeout(function(){
-        $('.flash-messages > .alert').fadeOut('slow', function(){
-          $(this).remove();
-        });
-      }, 3500);
-
-      return $('.flash-messages').append(JST["templates/shared/msg"]({
-        object: comment
-      }));
-    }
-
-  });
-
-  // answer comment create
-  PrivatePub.subscribe('/answers/' + questionId + '/comments/create', function(data, channel) {
     var comment = $.parseJSON(data['comment']);
     var parentType = comment.relationships.commentable.commentable_type
     var parentId = comment.relationships.commentable.commentable_id
     var authorId = comment.relationships.author.author_id
     var CommentsDiv = $('#'+parentType+'_'+parentId+'_comments');
-
     authorId == userId ? comment.currentUserIsAuthor = true : comment.currentUserIsAuthor = false;
-
     newCommentDiv = JST["templates/comments/comment"]({object: comment});
     CommentsDiv.find('.'+parentType+'_comments').prepend(newCommentDiv);
     CommentsDiv.find('.commentMessages').html('');
-
     if (comment.currentUserIsAuthor) {
       $('.'+parentType+'_'+parentId+'_comment_content').val('');
-
       setTimeout(function(){
         $('.flash-messages > .alert').fadeOut('slow', function(){
           $(this).remove();
         });
       }, 3500);
-
       return $('.flash-messages').append(JST["templates/shared/msg"]({
         object: comment
       }));
     }
-  });
+
+  }
 
 
+  function removeCommentFromCommentable(data){
+    var comment = $.parseJSON(data['comment']);
+    var parentType = comment.relationships.commentable.commentable_type
+    var authorId = comment.relationships.author.author_id
+    authorId == userId ? comment.currentUserIsAuthor = true : comment.currentUserIsAuthor = false;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // question comment destroy
-  PrivatePub.subscribe('/questions/' + questionId + '/comments/destroy', function(data, channel) {
-
-    var comment;
-    comment = $.parseJSON(data['comment']);
-    comment.author.author_id == userId ? comment.currentUserIsAuthor = true : comment.currentUserIsAuthor = false;
-
-    $('.question_comments').find('#'+comment.parent_type+'_comment_'+comment.id).fadeOut('fast', function(){
+    $('#'+parentType+'_comment_'+comment.id).fadeOut('fast', function(){
       $(this).remove();
     });
 
@@ -106,32 +53,28 @@ ready = function() {
         object: comment
       }));
     }
+  }
+
+  // question comment create
+  PrivatePub.subscribe('/questions/' + questionId + '/comments/create', function(data, channel) {
+    return addCommentToCommentable(data);
+  });
+
+  // answer comment create
+  PrivatePub.subscribe('/answers/' + questionId + '/comments/create', function(data, channel) {
+    return addCommentToCommentable(data);
+  });
+  
+
+  // question comment destroy
+  PrivatePub.subscribe('/questions/' + questionId + '/comments/destroy', function(data, channel) {
+    return removeCommentFromCommentable(data);
   });
 
 
   // answer comment destroy
   PrivatePub.subscribe('/answers/' + questionId + '/comments/destroy', function(data, channel) {
-
-    var comment;
-    comment = $.parseJSON(data['comment']);
-    comment.author.author_id == userId ? comment.currentUserIsAuthor = true : comment.currentUserIsAuthor = false;
-
-    $('#'+comment.parent_type+'_comment_'+comment.id).fadeOut('fast', function(){
-      $(this).remove();
-    });
-
-    if (comment.currentUserIsAuthor) {
-
-      setTimeout(function(){
-        $('.flash-messages > .alert').fadeOut('slow', function(){
-          $(this).remove();
-        });
-      }, 3500);
-
-      return $('.flash-messages').append(JST["templates/shared/msg"]({
-        object: comment
-      }));
-    }
+    return removeCommentFromCommentable(data);
   });
 
 
--- app/views/comments/destroy.json.jbuilder
+++ app/views/comments/destroy.json.jbuilder
@@ -13,3 +13,22 @@ json.msgs do
   json.error_msg flash['error']
   json.ok_msg flash['success']
 end
+
+json.type 'comments'
+
+json.attributes do
+  json.created_at   @comment.created_at.strftime('%H:%M:%S %Y-%m-%d %Z')
+  json.content      @comment.content
+end
+
+json.relationships do
+  json.author do
+    json.author_id @comment.user_id
+    json.author_name @comment.user.username
+  end
+
+  json.commentable do
+    json.commentable_type @comment.commentable_type.downcase
+    json.commentable_id   @comment.commentable_id
+  end
+end
