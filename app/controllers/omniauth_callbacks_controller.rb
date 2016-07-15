ca225f457f94d536a3058a9b142466422f62bc7b
Badalloff <badalloffsh@gmail.com>
2016-07-16 01:44:57 +0400

    Add: Twitter gem and devise config + app id and key

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -6,12 +6,16 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   end
 
+  def twitter
+
+  end
+
   private
 
   def oauth
-    @user = User.find_for_oauth(auth)
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
+    user = User.find_for_oauth(auth)
+    if user && user.persisted?
+      sign_in_and_redirect user, event: :authentication
       set_flash_message(:notice, :success, kind: "#{action_name}".capitalize) if is_navigational_format?
     end
   end
