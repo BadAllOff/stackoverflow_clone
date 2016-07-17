a41a5c719c5068ab883a0ef6fec689069473eff4
Badalloff <badalloffsh@gmail.com>
2016-07-17 23:25:53 +0400

    Fix: user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe User do
     it { should validate_presence_of :password }
     it { should validate_presence_of :username }
     it { should validate_uniqueness_of(:email).case_insensitive }
-    it { should validate_uniqueness_of(:username) }
+    it { should validate_uniqueness_of(:username).case_insensitive }
   end
 
   let(:user) { create(:user) }
