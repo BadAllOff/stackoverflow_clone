0affa070d9e0335d75c16e20c3c6b886c271848b
Badalloff <badalloffsh@gmail.com>
2016-08-05 15:15:26 +0400

    Refactor: answer controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -84,6 +84,14 @@ RSpec.describe QuestionsController, type: :controller do
           expect { post :create, question: attributes_for(:question) }.to change(@user.questions, :count).by(1)
         end
 
+        it '- subscribes author to question' do
+          expect { post :create, question: attributes_for(:question) }.to change(@user.subscriptions, :count).by(1)
+        end
+
+        it '- creates subscription to question' do
+          expect { post :create, question: attributes_for(:question) }.to change(Subscription, :count).by(1)
+        end
+
         it '- redirects to show view' do
           post :create, question: attributes_for(:question)
           expect(response).to redirect_to question_path(assigns(:question))
