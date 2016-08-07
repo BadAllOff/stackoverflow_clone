21829fcb4d9cab20b1439b3ee8b48a7b39e613ea
Badalloff <badalloffsh@gmail.com>
2016-08-07 16:51:50 +0400

    Refactor: unnecessary eager loading if user not signed in

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe AnswersController, type: :controller do
         it '- does not save the answer' do
           expect{ post :create, answer: attributes_for(:invalid_answer), question_id: question, format: :json }.to_not change(Answer, :count)
         end
-        it '- returns errors in json format' do
+        it '- when not valid' do
           post :create, answer: attributes_for(:invalid_answer), question_id: question, format: :json
           expect(response).to render_template 'answers/errors.json.jbuilder'
           expect(response.status).to eq 400
@@ -50,7 +50,7 @@ RSpec.describe AnswersController, type: :controller do
     end
 
     context 'Non-authenticated user try to create answer' do
-      it '- sends back status unauthorized' do
+      it '- get\'s status unauthorized' do
         expect{ post :create, answer: attributes_for(:answer), question_id: question, format: :json }.to_not change(question.answers, :count)
         expect(response.status).to eq 401
       end
@@ -63,7 +63,7 @@ RSpec.describe AnswersController, type: :controller do
       sign_in_user
       context 'operates with his own answer' do
         let!(:answer) { create(:answer, question: question, user: @user) }
-        context 'with valid attributes' do
+        context 'when valid' do
           it '- assigns the requested answer to @answer' do
             patch :update, question_id: question, id: answer, answer: attributes_for(:answer), format: :json
             expect(assigns(:answer)).to eq answer
@@ -81,7 +81,7 @@ RSpec.describe AnswersController, type: :controller do
           end
         end
 
-        context 'with invalid attributes' do
+        context 'when invalid' do
           before { patch :update, question_id: question, id: answer, answer: { body: nil }, format: :json }
           it '- does not change answer attributes' do
             answer.reload
@@ -150,7 +150,7 @@ RSpec.describe AnswersController, type: :controller do
       end
 
 
-      context 'operates with another users question answer' do
+      context 'operates with another users answer' do
         sign_in_another_user
         let!(:question) { create(:question, user: user) }
         let!(:answer) { create(:answer, question: question, user: user) }
