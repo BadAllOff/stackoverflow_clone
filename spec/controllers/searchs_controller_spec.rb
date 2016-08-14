dbdeb37c574d2d684f4af64c1d75508819fac7a2
Badalloff <badalloffsh@gmail.com>
2016-08-14 17:55:26 +0400

    Refactor: search controller test

--- spec/controllers/searchs_controller_spec.rb
+++ spec/controllers/searchs_controller_spec.rb
@@ -3,36 +3,23 @@ require 'rails_helper'
 RSpec.describe SearchsController, type: :controller do
 
   describe 'GET #index' do
-    it 'returns http success' do
+    it '- returns http success' do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'POST #results' do
-    it 'returns http success' do
+    it '- returns http success' do
       post :results, search_query: {query: 'question', index_type: 'nil'}
       expect(response).to have_http_status(:success)
     end
 
-    it 'returns http success' do
-      post :results, search_query: {query: 'question', index_type: 'Questions'}
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'returns http success' do
-      post :results, search_query: {query: 'answer', index_type: 'Answers'}
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'returns http success' do
-      post :results, search_query: {query: 'comment', index_type: 'Comments'}
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'returns http success' do
-      post :results, search_query: {query: 'username', index_type: 'Users'}
-      expect(response).to have_http_status(:success)
+    %w(question answer comment user).each do |attr|
+      it "- returns http success for index #{attr.pluralize.capitalize}" do
+        post :results, search_query: {query: attr, index_type: "#{attr.pluralize.capitalize}" }
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 
